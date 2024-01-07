71b625f0795de4bfa334f4c4e6e96c5ddeab96d8
kunalgit25 <146455946+kunalgit25@users.noreply.github.com>
2024-01-07 10:05:43 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,28 @@
+public final class Program {
+    
+    
+    public int CDE ;
+
+    public int ABC ;
+
+
+
+    public Program(int C, int A){
+
+         CDE = C;
+
+         ABC = A;
+
+    }
+
+    public int AddSum(){
+
+        int sum;
+
+        sum = CDE + ABC ;
+
+        return sum ;
+    }
+
+
+}
