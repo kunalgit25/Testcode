9651f3976957978359ed1314830cf32ef3b70f35
kunalgit25 <146455946+kunalgit25@users.noreply.github.com>
2024-01-07 10:10:21 +0530

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,15 @@
+public class Program2{
+
+    public static void main(String[] args) {
+        int a=10;
+        int b=70;
+     Program obj = new Program(a,b);
+    
+     obj.AddSum();
+
+     System.out.println(obj.AddSum());
+
+
+
+    }
+}
